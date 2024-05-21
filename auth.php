27c8ebea239a32d5c90dd7a583104e2fe3f06c19
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2024-05-21 13:13:31 -0500

    Update the error managment with the bad request

--- auth.php
+++ auth.php
@@ -7,13 +7,34 @@ $_auth = new auth();
 
 $_respuestas = new respuesta();
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+//validamos que el metodo que se este utilizando sea POST
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){ 
+    //recibimos los datos enviados 
     $postbody = file_get_contents("php://input");
+    //enviamos los datos al manejador
     $datosArray = $_auth->login($postbody);
-    print_r(json_encode($datosArray));
+    //delvovemos una respuesta
+    header('Content-Type: application/json');
+    //si en la respuesta que nos devolvio el manejador existe un error lo mostramos
+    if(isset($datosArray["result"]["error_id"])){
+        //si existe un error le colocamos el codigo de error correspondiente
+        $responseCode = $datosArray["result"]["error_id"];
+        //colocamos el codigo de error
+        http_response_code($responseCode);
+    }else{
+        //si no hay errores le colocamos el codigo 200 que es de OK
+        http_response_code(200);
+    }
+    //mostramos la respuesta en formato json
+    echo json_encode($datosArray);
 
     
 
 }else{
-    echo "Metodo no permitido";
+    //si no es metodo POST mostramos un error
+    header('Content-Type: application/json');
+    //mostramos una respuesta de metodo no permitido
+    $datosArray = $_respuestas->error_405();
+    echo json_encode($datosArray);
 }
