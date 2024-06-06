5a7c56c18bd3b776352e16e1cbe5e2743125b707
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2024-06-05 22:34:08 -0500

    we are done with the api

--- clases/pacientes.class.php
+++ clases/pacientes.class.php
@@ -1,47 +1,232 @@
 <?php
+require_once "conexion/conexion.php";
+require_once "respuestas.class.php";
 
-require_once 'conexion/conexion.php';
-require_once 'respuestas.class.php';
 
-//Esta clase se encarga de manejar los pacientes
-//Extiende de la clase conexion
-class pacientes extends conexion{
+class pacientes extends conexion {
+
     private $table = "pacientes";
-    //esta funcion se encarga de mostrar los pacientes de la base de datos
+    private $pacienteid = "";
+    private $dni = "";
+    private $nombre = "";
+    private $direccion = "";
+    private $codigoPostal = "";
+    private $genero = "";
+    private $telefono = "";
+    private $fechaNacimiento = "0000-00-00";
+    private $correo = "";
+    private $token = "";
+//912bc00f049ac8464472020c5cd06759
 
     public function listaPacientes($pagina = 1){
-        $inicio = 0;
+        $inicio  = 0 ;
         $cantidad = 100;
-        //validamos la cantidad de registros que se van a mostrar por hoja
-        if($pagina>1){
-            //aqui mostramos los registros que se van a mostrar por pagina de 100 en 100
-            $inicio = ($cantidad * ($pagina-1))+1;
+        if($pagina > 1){
+            $inicio = ($cantidad * ($pagina - 1)) +1 ;
             $cantidad = $cantidad * $pagina;
-
         }
-        //esta query es para obtener los datos de la base de datos
-        $query = "SELECT PacienteId,DNI,Nombre,Telefono,Correo FROM " . 
-        $this->table . " LIMIT $inicio, $cantidad";
-        //obtenemos los datos de la base de datos
+        $query = "SELECT PacienteId,Nombre,DNI,Telefono,Correo FROM " . $this->table . " limit $inicio,$cantidad";
         $datos = parent::obtenerDatos($query);
-        //retornamos los datos
         return ($datos);
-
-        //la url que usamos es esta 
-        //localhost/Proyectos/proyectoGitHub/pacientes?page=1
-
-        
     }
-    
-    //esta funcion se encarga de obtener un paciente en especifico
+
     public function obtenerPaciente($id){
-        $query = "SELECT * FROM " . 
-        $this->table . " WHERE PacienteId = '$id'";
+        $query = "SELECT * FROM " . $this->table . " WHERE PacienteId = '$id'";
         return parent::obtenerDatos($query);
 
-        //la url que usamos es esta
-        //localhost/Proyectos/proyectoGitHub/pacientes?id=1
     }
+
+    public function post($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json,true);
+
+        if(!isset($datos['token'])){
+                return $_respuestas->error_401();
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken =   $this->buscarToken();
+            if($arrayToken){
+
+                if(!isset($datos['nombre']) || !isset($datos['dni']) || !isset($datos['correo'])){
+                    return $_respuestas->error_400();
+                }else{
+                    $this->nombre = $datos['nombre'];
+                    $this->dni = $datos['dni'];
+                    $this->correo = $datos['correo'];
+                    if(isset($datos['telefono'])) { $this->telefono = $datos['telefono']; }
+                    if(isset($datos['direccion'])) { $this->direccion = $datos['direccion']; }
+                    if(isset($datos['codigoPostal'])) { $this->codigoPostal = $datos['codigoPostal']; }
+                    if(isset($datos['genero'])) { $this->genero = $datos['genero']; }
+                    if(isset($datos['fechaNacimiento'])) { $this->fechaNacimiento = $datos['fechaNacimiento']; }
+                    $resp = $this->insertarPaciente();
+                    if($resp){
+                        $respuesta = $_respuestas->response;
+                        $respuesta["result"] = array(
+                            "pacienteId" => $resp
+                        );
+                        return $respuesta;
+                    }else{
+                        return $_respuestas->error_500();
+                    }
+                }
+
+            }else{
+                return $_respuestas->error_401("El Token que envio es invalido o ha caducado");
+            }
+        }
+
+
+       
+
+    }
+
+
+    private function insertarPaciente(){
+        $query = "INSERT INTO " . $this->table . " (DNI,Nombre,Direccion,CodigoPostal,Telefono,Genero,FechaNacimiento,Correo)
+        values
+        ('" . $this->dni . "','" . $this->nombre . "','" . $this->direccion ."','" . $this->codigoPostal . "','"  . $this->telefono . "','" . $this->genero . "','" . $this->fechaNacimiento . "','" . $this->correo . "')"; 
+        $resp = parent::nonQueryId($query);
+        if($resp){
+             return $resp;
+        }else{
+            return 0;
+        }
+    }
+    
+    public function put($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json,true);
+
+        if(!isset($datos['token'])){
+            return $_respuestas->error_401();
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken =   $this->buscarToken();
+            if($arrayToken){
+                if(!isset($datos['pacienteId'])){
+                    return $_respuestas->error_400();
+                }else{
+                    $this->pacienteid = $datos['pacienteId'];
+                    if(isset($datos['nombre'])) { $this->nombre = $datos['nombre']; }
+                    if(isset($datos['dni'])) { $this->dni = $datos['dni']; }
+                    if(isset($datos['correo'])) { $this->correo = $datos['correo']; }
+                    if(isset($datos['telefono'])) { $this->telefono = $datos['telefono']; }
+                    if(isset($datos['direccion'])) { $this->direccion = $datos['direccion']; }
+                    if(isset($datos['codigoPostal'])) { $this->codigoPostal = $datos['codigoPostal']; }
+                    if(isset($datos['genero'])) { $this->genero = $datos['genero']; }
+                    if(isset($datos['fechaNacimiento'])) { $this->fechaNacimiento = $datos['fechaNacimiento']; }
+        
+                    $resp = $this->modificarPaciente();
+                    if($resp){
+                        $respuesta = $_respuestas->response;
+                        $respuesta["result"] = array(
+                            "pacienteId" => $this->pacienteid
+                        );
+                        return $respuesta;
+                    }else{
+                        return $_respuestas->error_500();
+                    }
+                }
+
+            }else{
+                return $_respuestas->error_401("El Token que envio es invalido o ha caducado");
+            }
+        }
+
+
+    }
+
+
+    private function modificarPaciente(){
+        $query = "UPDATE " . $this->table . " SET Nombre ='" . $this->nombre . "',Direccion = '" . $this->direccion . "', DNI = '" . $this->dni . "', CodigoPostal = '" .
+        $this->codigoPostal . "', Telefono = '" . $this->telefono . "', Genero = '" . $this->genero . "', FechaNacimiento = '" . $this->fechaNacimiento . "', Correo = '" . $this->correo .
+         "' WHERE PacienteId = '" . $this->pacienteid . "'"; 
+        $resp = parent::nonQuery($query);
+        if($resp >= 1){
+             return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+    public function delete($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json,true);
+
+        if(!isset($datos['token'])){
+            return $_respuestas->error_401();
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken =   $this->buscarToken();
+            if($arrayToken){
+
+                if(!isset($datos['pacienteId'])){
+                    return $_respuestas->error_400();
+                }else{
+                    $this->pacienteid = $datos['pacienteId'];
+                    $resp = $this->eliminarPaciente();
+                    if($resp){
+                        $respuesta = $_respuestas->response;
+                        $respuesta["result"] = array(
+                            "pacienteId" => $this->pacienteid
+                        );
+                        return $respuesta;
+                    }else{
+                        return $_respuestas->error_500();
+                    }
+                }
+
+            }else{
+                return $_respuestas->error_401("El Token que envio es invalido o ha caducado");
+            }
+        }
+
+
+
+     
+    }
+
+
+    private function eliminarPaciente(){
+        $query = "DELETE FROM " . $this->table . " WHERE PacienteId= '" . $this->pacienteid . "'";
+        $resp = parent::nonQuery($query);
+        if($resp >= 1 ){
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+    private function buscarToken(){
+        $query = "SELECT  TokenId,UsuarioId,Estado from usuarios_token WHERE Token = '" . $this->token . "' AND Estado = 'Activo'";
+        $resp = parent::obtenerDatos($query);
+        if($resp){
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+    private function actualizarToken($tokenid){
+        $date = date("Y-m-d H:i");
+        $query = "UPDATE usuarios_token SET Fecha = '$date' WHERE TokenId = '$tokenid' ";
+        $resp = parent::nonQuery($query);
+        if($resp >= 1){
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
 
 
 }
+
+
+
+
+
+?>
--- clases/respuestas.class.php
+++ clases/respuestas.class.php
@@ -1,10 +1,12 @@
-<?php
+<?php 
 
-class respuesta{
-    public $response =[
+class respuestas{
+
+    public  $response = [
         'status' => "ok",
         "result" => array()
     ];
+
 
     public function error_405(){
         $this->response['status'] = "error";
@@ -12,40 +14,50 @@ class respuesta{
             "error_id" => "405",
             "error_msg" => "Metodo no permitido"
         );
-        $response = $this->response;
-        return $response;
-        
+        return $this->response;
     }
 
-    public function error_200($string = "Datos incorrectos"){
+    public function error_200($valor = "Datos incorrectos"){
         $this->response['status'] = "error";
         $this->response['result'] = array(
             "error_id" => "200",
-            "error_msg" => "$string"
+            "error_msg" => $valor
         );
-        $response = $this->response;
-        return $response;
-        
+        return $this->response;
     }
+
 
     public function error_400(){
         $this->response['status'] = "error";
         $this->response['result'] = array(
             "error_id" => "400",
-            "error_msg" => "Datos enviaddos incompletos o con formato incorrecto"
+            "error_msg" => "Datos enviados incompletos o con formato incorrecto"
         );
-        $response = $this->response;
-        return $response;
-        
+        return $this->response;
     }
-    public function error_500($string = "Error interno del servidor"){
+
+
+    public function error_500($valor = "Error interno del servidor"){
         $this->response['status'] = "error";
         $this->response['result'] = array(
             "error_id" => "500",
-            "error_msg" => "$string"
+            "error_msg" => $valor
         );
-        $response = $this->response;
-        return $response;
-        
+        return $this->response;
     }
+
+
+    public function error_401($valor = "No autorizado"){
+        $this->response['status'] = "error";
+        $this->response['result'] = array(
+            "error_id" => "401",
+            "error_msg" => $valor
+        );
+        return $this->response;
+    }
+    
+    
+
 }
+
+?>
