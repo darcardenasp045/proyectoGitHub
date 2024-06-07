895d8e17a771bfd87205b746d8a8778e3d0e1857
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2024-06-07 17:46:50 -0500

    update cors

--- auth.php
+++ auth.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://consumir-api-php.vercel.app/"); // Permite solicitudes desde este origen
+header("Access-Control-Allow-Origin: https://consumir-api-php.vercel.app"); // Permite solicitudes desde este origen
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeceras permitidas
 
