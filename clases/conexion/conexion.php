73694d8e059c2eb5317680d3779f61c76dc9005c
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2024-06-07 17:18:31 -0500

    update mb-coding

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -41,7 +41,7 @@ class conexion{
     //creamos una funcion para convertir los datos a utf8
     private function convertirUTF8($array){
         array_walk_recursive($array, function(&$item, $key){
-            if(!mb_detect_encoding($item, 'utf-8', true)){
+            if($item !== null && !mb_detect_encoding($item, 'utf-8', true)){
                 $item = mb_convert_encoding($item, 'UTF-8', 'ISO-8859-1');
             }
         });
